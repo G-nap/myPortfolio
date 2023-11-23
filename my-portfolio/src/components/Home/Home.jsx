2fc953749ed2cfde2e8094f4289c735c0faf780a
Chotika T <54322772+G-nap@users.noreply.github.com>
2023-11-24 05:13:27 +0700

    add home

--- my-portfolio/src/components/Home/Home.jsx
+++ my-portfolio/src/components/Home/Home.jsx
@@ -2,12 +2,35 @@ import React from 'react'
 import './Home.css'
 
 function Home() {
-  return (
-    <div className='home-flex-box'>
-      <div className='home-flex-item-text'></div>
-      <div className='home-flex-item-img'></div>
-    </div>
-  )
+    return (
+        <div className='home-flex-box'>
+            <div className='home-flex-item-text'>
+                <div className='home-text-p'>WELCOME TO MY PORTFOLIO</div>
+                <div className='home-text-h1'>
+                    <span style={{ color: '#3435CE' }}>Front-end&nbsp;</span>
+                    <span>Developer</span>
+                </div>
+                <div className='home-text-p'>Hi, I’m Chotika Trakulnuch. New graduate from
+                    Computer Engineering, KMITL. A passionate Front-end Developer.</div>
+                <div className='home-text-pB'>Tech Stack</div>
+                <div className='home-sm-box'>
+                    <div className='home-sm-box-item'></div>
+                    <div className='home-sm-box-item'></div>
+                    <div className='home-sm-box-item'></div>
+                    <div className='home-sm-box-item'></div>
+                </div>
+            </div>
+            <div className='home-flex-item-img'>
+                <div className='home-img-layout'>
+                    <img div className='home-img'/>
+                </div>
+                <div className='home-sm-box'>
+                    <div className='home-sm-box-item'></div>
+                    <div className='home-sm-box-item'></div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
