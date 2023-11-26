a22265dadc8c81b5e690c5249b67cb6b4c6937ee
Chotika T <54322772+G-nap@users.noreply.github.com>
2023-11-27 06:21:08 +0700

    css text animation

--- my-portfolio/src/components/Home/Home.jsx
+++ my-portfolio/src/components/Home/Home.jsx
@@ -6,8 +6,8 @@ function Home() {
         <div className='home-flex-box'>
             <div className='home-flex-item-text'>
                 <div className='home-text-p'>WELCOME TO MY PORTFOLIO</div>
-                <div className='home-text-h1'>
-                    <span style={{ color: '#3435CE' }}>Front-end&nbsp;</span>
+                <div className='home-text-h1 typewriter'>
+                    <h4 style={{ color: '#3435CE' }}>Front-end&nbsp;</h4>
                     <span>Developer</span>
                 </div>
                 <div className='home-text-p'>Hi, I’m Chotika Trakulnuch. New graduate from
