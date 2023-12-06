ce6b73994d6cae27e89898aa7d598461f004dc09
Chotika T <54322772+G-nap@users.noreply.github.com>
2023-12-06 08:35:16 +0700

    add icon

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { VscMail } from 'react-icons/vsc'
+import { IoCallOutline } from 'react-icons/io5'
+import { CiLocationOn } from "react-icons/ci";
+import { SlLocationPin } from "react-icons/sl";
 
 function Contact() {
     return (
@@ -6,9 +10,9 @@ function Contact() {
             <div className=''>
                 <div className='flex justify-around items-center md:p-10 p-4 bg-[#f5f5ff] '>
                     <div className="text-[16px] text-slate-950 font-semibold font-roboto-mono hidden sm:block">  Chotie.Portfolio </div>
-                    <div className='font-roboto-mono md:text-xl text-sm md:col-span-2 '> BKK,Thailand </div>
-                    <div className='font-roboto-mono md:text-xl text-sm md:col-span-2 '> 089-046-6328 </div>
-                    <div className='font-roboto-mono md:text-xl text-sm md:col-span-2 '>chotika.tra@gmail.com </div>
+                    <div className='font-roboto-mono md:text-xl text-sm md:col-span-2 flex items-center gap-3'><SlLocationPin /> BKK,Thailand </div>
+                    <div className='font-roboto-mono md:text-xl text-sm md:col-span-2 flex items-center gap-3'><IoCallOutline />089-046-6328 </div>
+                    <div className='font-roboto-mono md:text-xl text-sm md:col-span-2 flex items-center gap-3'><VscMail />chotika.tra@gmail.com </div>
                 </div>
                 <div className='bg-[#212121] flex justify-center items-center text-[#8E8E8E] p-2'>
                     <div className='font-roboto-mono md:text-xl text-sm md:col-span-2 '>© 2023 Chotie.Portfolio</div>
